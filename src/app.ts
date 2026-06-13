64ef00a81eb78adc8d0984e001db605f1a64b0cc
Prakash <prakashrajbanc1@gmail.com>
2026-06-13 21:01:10 +0545

    User Middleware added

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express'
 const app = express()
 import userRoute from './routes/userRoute'
+import categoryRoute from './routes/categoryRoute'
 import "./database/connection"
 
 app.use(express.json())
 
 app.use("/api/auth", userRoute)
+app.use('/api/category', categoryRoute)
 
 export default app
 
--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -1,8 +1,9 @@
 import express, { Router } from 'express'
 import categoryController from '../controllers/categoryController'
+import userMiddleware from '../middleware/userMiddleware'
 const router:Router = express.Router()
 
-router.route("/").get(categoryController.getCategories).post(categoryController.addCategory)
-router.route("/:id").patch(categoryController.updateCategory).delete(categoryController.deleteCategory)
+router.route("/").get(categoryController.getCategories).post(userMiddleware.isUserLoggedIn, categoryController.addCategory)
+router.route("/:id").patch(userMiddleware.isUserLoggedIn, categoryController.updateCategory).delete(userMiddleware.isUserLoggedIn, categoryController.deleteCategory)
 
 export default router
